6edbef25470168b25bc0a658561f312ecd5c30ca
Emi-L-ramos <emiliesmaz@gmail.com>
2026-05-26 15:56:24 -0600

    no se2

--- Pescuela/Pescuela/settings.py
+++ Pescuela/Pescuela/settings.py
@@ -118,9 +118,6 @@ DATABASES = {
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
-            'ssl': {
-                'ca': '/etc/secrets/ca.pem',
-            },
         }
     }
 }
