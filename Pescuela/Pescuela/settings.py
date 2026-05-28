dc3a309b40affc32d71d116b77c27dce285b7c27
Emi-L-ramos <emiliesmaz@gmail.com>
2026-05-28 08:27:09 -0600

    Corrige ssl mysql aiven

--- Pescuela/Pescuela/settings.py
+++ Pescuela/Pescuela/settings.py
@@ -120,9 +120,7 @@ DATABASES = {
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
-            'ssl': {
-                'ca': '/etc/ssl/certs/ca-certificates.crt',
-            },
+            'ssl': {},
         }
     }
 }
