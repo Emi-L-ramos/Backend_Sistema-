6cdf9c2dd095a5364543e45bcfc34c3cf2cfa6ed
Emi-L-ramos <emiliesmaz@gmail.com>
2026-05-28 08:39:14 -0600

    Permite servir media en produccion

--- Pescuela/Pescuela/urls.py
+++ Pescuela/Pescuela/urls.py
@@ -19,7 +19,7 @@ from django.urls import include, path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,7 +29,9 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view()),
     path('api/token/refresh/', TokenRefreshView.as_view()),
     path('accounts/', include('django.contrib.auth.urls')),
+    path(
+        'media/<path:path>',
+        serve,
+        {'document_root': settings.MEDIA_ROOT}
+    ),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
